8e84b7befaf97060b19a31c6d4882e2af57d7ca2
zshunshun <zshunshun2018@gmail.com>
2020-03-27 19:50:11 +0800

    删除一个多余导包

--- user-client/src/main/java/com/shun/service/UserServiceImpl.java
+++ user-client/src/main/java/com/shun/service/UserServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tk.mybatis.mapper.entity.Example;
 
-import java.io.File;
 import java.io.OutputStream;
 import java.util.*;
 
