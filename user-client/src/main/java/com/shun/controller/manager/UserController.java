8856e17dd892a79a22ef828984026323b6d7e6b2
zshunshun <zshunshun2018@gmail.com>
2020-03-27 10:25:43 +0800

    优化静态文件引用逻辑

--- user-client/src/main/java/com/shun/controller/manager/UserController.java
+++ user-client/src/main/java/com/shun/controller/manager/UserController.java
@@ -7,11 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
